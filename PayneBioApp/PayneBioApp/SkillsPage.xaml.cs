d194395b3b66f084ced76388f3676689b5d4caf4
0753409 <0753409@KELCE-ACIS18-P>
2018-10-29 18:20:29 -0500

    Added skills page and photos page. Made changes to contact form

--- PayneBioApp/PayneBioApp/SkillsPage.xaml.cs
+++ PayneBioApp/PayneBioApp/SkillsPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +10,25 @@ using Xamarin.Forms.Xaml;
 
 namespace PayneBioApp
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class SkillsPage : ContentPage
-	{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SkillsPage : ContentPage
+    {
+        public ObservableCollection<String> Skills { get; set; }
 		public SkillsPage ()
 		{
 			InitializeComponent ();
-		}
+            this.Skills = new ObservableCollection<string>();
+            this.Skills.Add("C+");
+            this.Skills.Add("Python");
+            this.Skills.Add("VBA");
+            this.Skills.Add("Xamarin");
+            this.Skills.Add("C#");
+            this.Skills.Add("BS in Mathematics");
+            this.Skills.Add("");
+            SkillsListView.ItemsSource = this.Skills;
+
+
+        }
 	}
+    
 }
